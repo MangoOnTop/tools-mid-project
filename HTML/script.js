2daf48223f91c16d25b9365cbd2d5a1f29415f0a
Nasim-228 <nasimulibnrashid@gmail.com>
2026-05-10 11:59:08 +0600

    sliding has been done

--- HTML/script.js
+++ HTML/script.js
@@ -1,12 +1,18 @@
 const right_arrow = document.getElementById("right_arrow");
+const left_arrow = document.getElementById("left_arrow");
 const slideer = document.getElementById("slider");
 let count = 0;
 
-function slide() {
-  if (count > -300) {
+function slide(e) {
+  if (e.target.id == "right_arrow" && count != -300) {
     count -= 100;
+    slideer.style.transform = `translateX(${count}%)`;
+  }
+  if (e.target.id == "left_arrow" && count != 0) {
+    count += 100;
     slideer.style.transform = `translateX(${count}%)`;
   }
 }
 
 right_arrow.addEventListener("click", slide);
+left_arrow.addEventListener("click", slide);
